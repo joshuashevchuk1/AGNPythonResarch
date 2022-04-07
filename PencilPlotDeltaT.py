568a14eebbadae3a1b9f1c10abbd7da7e84746b3
joshuashevchuk1 <joshuashevchuk1>
2022-04-07 00:30:27 -0700

    proper loop and get deltaT

--- PencilPlotDeltaT.py
+++ PencilPlotDeltaT.py
@@ -6,45 +6,44 @@ import os
 from pylab import *
 import sys
 
+CONST_INTERVAL=5
+max_orbits = 500
+inital_ivar = 0
 
 def plots():
     root = os.getcwd()  # root dir is fucked up due to a space
     dir_run_list = next(os.walk('.'))[1]
-    max_orbits = 500
-    ivar = 0
-    while ivar <= max_orbits:
-        plotRuns(dir_run_list, ivar, root)  # run correct python file and save plots locally
-        ivar = ivar + 1
-    os.chdir(root)
-
-
-def plotRuns(dir_run_list, ivar, root):
-    print("============")
-    print("entering plotRuns")
-    d_tarray = []
     for i in range(len(dir_run_list)):
         print("dir_run_list is " + dir_run_list[i])
         os.chdir(dir_run_list[i])
         print("current cwd is " + os.path.split(os.getcwd())[1])
         try:
-            getRunData(ivar, d_tarray)
+            plots(inital_ivar)
             os.chdir(root)
         except:
             print("============")
             print("ignoring run")
             print("============")
             os.chdir(root)
+    os.chdir(root)
 
+def plotRuns(ivar):
+    print("============")
+    print("entering plotRuns")
+    DTarray=[]
+    while ivar <= max_orbits:
+        DTarray = getRunData(ivar,DTarray)
+        ivar = ivar + CONST_INTERVAL
+    plotCollectedData(DTarray)
     print("leaving plotRuns")
     print("============")
-
 
 def getRunData(ivar, paramDTarray):
     ff = pc.read_var(trimall=True, ivar=ivar, magic=["TT"])
     ff0 = pc.read_var(trimall=True, ivar=0, magic=["TT"])
     dfT = ff.TT[:] - ff0.TT[:]
     paramDTarray.append(np.log(np.sum(dfT ** 2, axis=0)))
-
+    return paramDTarray
 
 def plotCollectedData(paramDTarray):
     print("entering plotCollectedData")
@@ -58,6 +57,5 @@ def plotCollectedData(paramDTarray):
     plt.savefig(name + "-log-" + str(ivar) + ".png")
     plt.close()
     print("leaving plotCollectedData")
-
 
 plots()
