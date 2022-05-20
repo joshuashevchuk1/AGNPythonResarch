48ecc0aeca0f0f9a0fec8f544e233bd3a5e31d7e
joshuashevchuk1 <joshuashevchuk1>
2022-05-20 00:15:35 -0700

    label was wrong

--- PencilAnalysis.py
+++ PencilAnalysis.py
@@ -2170,7 +2170,7 @@ class Pencil_Analysis(object):
                     Dir_beta_patches = mpatches.Patch(
                         color='white', label=r'$\beta$ :'+str(beta))
                     Dir_cutoff_patches = mpatches.Patch(
-                        color='white', label=r'$\beta$ :'+str(cut_off))
+                        color='white', label=r'$t_{c}$ :'+str(time[cut_off]))
 
                     plt.legend(handles=[DirMass_patches,
                                         DirEcc_patches,
