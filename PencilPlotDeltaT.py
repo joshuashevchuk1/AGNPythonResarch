e6c409d0208233fb3592e3dafac5a224b2ea9e52
joshuashevchuk1 <joshuashevchuk1>
2022-06-02 11:02:00 -0700

    not numpy arrays

--- PencilPlotDeltaT.py
+++ PencilPlotDeltaT.py
@@ -103,9 +103,9 @@ def plotCollectedData(paramDTarray, dir):
     plt.plot(paramDTarray)
     plt.grid(True)
     Dir_max_patches = mpatches.Patch(
-        color='white', label=r'$t_{max}$ :' + paramDTarray.max())
+        color='white', label=r'$t_{max}$ :' + max(paramDTarray))
     Dir_min_patches = mpatches.Patch(
-        color='white', label=r'$t_{min}$ :' + paramDTarray.min())
+        color='white', label=r'$t_{min}$ :' + min(paramDTarray))
     plt.legend(handles=[Dir_max_patches,Dir_min_patches], loc=2)
     plt.title('q = ' + str(q) + ',' + r'$\varepsilon$ = ' + str(ecc_int))
     plt.xlabel(r'$t/T_0$')
