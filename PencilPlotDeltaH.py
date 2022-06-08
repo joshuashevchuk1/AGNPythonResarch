86f34df224ccc792d1585ab01c67b9945782fd87
joshuashevchuk1 <joshuashevchuk1>
2022-06-07 23:23:28 -0700

    more checking on issues

--- PencilPlotDeltaH.py
+++ PencilPlotDeltaH.py
@@ -112,6 +112,8 @@ def getRunData(ivar, paramDTarray):
     dfT = ff.TT[:] - ff0.TT[:]
     T = np.max(np.log(np.sum(dfT ** 2, axis=0)))
     H,gamma = getAspectRatio()
+    print('T is ',T)
+    print('np.sum(dfT ** 2, axis=0)) is ', np.sum(dfT ** 2, axis=0))
     print('H is ',H)
     print('gamma is ',gamma)
     print('gamma*T/H is',gamma*T/H)
