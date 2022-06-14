8430b1d2d1143f23f9651669ad04ee515d589c4e
joshuashevchuk1 <joshuashevchuk1>
2022-06-14 02:00:04 -0700

    helper issues

--- PencilPlotDeltaT.py
+++ PencilPlotDeltaT.py
@@ -100,9 +100,9 @@ def plotCollectedData(paramDTarray, dir):
     plt.plot(paramDTarray)
     plt.grid(True)
     Dir_max_patches = mpatches.Patch(
-        color='white', label=r'$t_{max}$ :' + max(paramDTarray))
+        color='white', label=r'$t_{max}$ :' + str(max(paramDTarray)))
     Dir_min_patches = mpatches.Patch(
-        color='white', label=r'$t_{min}$ :' + min(paramDTarray))
+        color='white', label=r'$t_{min}$ :' + str((paramDTarray)))
     plt.legend(handles=[Dir_max_patches,Dir_min_patches], loc=2)
     plt.title('q = ' + str(q) + ',' + r'$\varepsilon$ = ' + str(ecc_int))
     plt.xlabel(r'$t/T_0$')
