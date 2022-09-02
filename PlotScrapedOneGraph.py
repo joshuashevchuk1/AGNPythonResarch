a06f7e57836aaefd3c5b29ee30c2b1dd4e681397
joshuashevchuk1 <joshuashevchuk1>
2022-09-01 23:38:52 -0700

    scrape!

---
+++ PlotScrapedOneGraph.py
@@ -0,0 +1,15 @@
+import pencil_old as pc
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import numpy as np
+import os
+from pylab import *
+import sys
+import traceback
+import json
+
+scrapeDict={}
+
+def getScrapedJson():
+
+    return none
