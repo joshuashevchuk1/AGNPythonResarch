6909caf04b4881ce1272a63b5eaadcdef2276a9e
joshuashevchuk1 <joshuashevchuk1>
2022-09-02 00:10:22 -0700

    try to plot them all

--- PlotScrapedOneGraph.py
+++ PlotScrapedOneGraph.py
@@ -9,13 +9,33 @@ import traceback
 import json
 
 scrapeDict={}
-max_orbits = 0
+max_orbits = int(input('max Orbits? : '))
 
-def setMaxOrbits():
-    return none
+
+def plotCollectedData(paramDTarray):
+    print("entering plotCollectedData")
+    print("paramDTArray is ",paramDTarray)
+    plt.plot(paramDTarray)
+    print("leaving plotCollectedData")
 
 def setScrapedJson():
     global scrapeDict
     with open('ScrapeDeltaTData_At'+str(max_orbits)+'.json', 'r') as f:
         scrapeDict = json.loads(f)
 
+def plot():
+    global scrapeDict
+
+    for i in range(len(scrapeDict)):
+        plt.plot(scrapeDict[i]["DTarray"], label= r"$\varepsilon$ = " + str(scrapeDict[i]["ecc_int"]))
+
+    plt.grid(True)
+    plt.legend()
+    plt.title('data')
+    plt.xlabel(r'$t/T_0$')
+    plt.ylabel('Temperature')
+    plt.tight_layout()
+    plt.savefig("ScrapedDeltaData_" + str(max_orbits) + "_plot" + ".png")
+    plt.close()
+
+    print("entering plot")
