14b6459df5ef12664c33e4e515414ddea8ddd818
joshuashevchuk1 <joshuashevchuk1>
2023-03-26 18:21:49 -0700

    cividis

--- ContourShockHeatingCheck.py
+++ ContourShockHeatingCheck.py
@@ -17,13 +17,13 @@ rad2d, theta2d = np.meshgrid(rad, theta)
 x2d = rad2d*np.cos(theta2d)
 y2d = rad2d*np.sin(theta2d)
 
-T0 = np.mean(np.transpose(ff.TT),axis=1)
+T0 = np.mean(np.transpose(f0.TT),axis=1)
 TT = np.mean(np.transpose(ff.TT),axis=1)
 
 print('T0 : ',T0)
 print('TT : ',TT)
 
-plt.plot(rad,T0)
-plt.plot(rad,TT)
+plt.plot(rad,T0,label='T0')
+plt.plot(rad,TT,label='TT)
 plt.show()
 
