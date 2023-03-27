3144b0cf354b5a0b0e4b615dc212f563b03ddd19
joshuashevchuk1 <joshuashevchuk1>
2023-03-26 18:19:43 -0700

    cividis

--- ContourShockHeatingCheck.py
+++ ContourShockHeatingCheck.py
@@ -7,7 +7,7 @@ import sys
 
 Orbits = int(input('Orbits?:'))
 
-f0=pc.read_var(trimall=True,ivar=Orbits,magic=['TT'])
+f0=pc.read_var(trimall=True,ivar=0,magic=['TT'])
 ff = pc.read_var(trimall=True, ivar=Orbits,magic=['TT'])
 
 rad = ff.x
@@ -17,11 +17,11 @@ rad2d, theta2d = np.meshgrid(rad, theta)
 x2d = rad2d*np.cos(theta2d)
 y2d = rad2d*np.sin(theta2d)
 
-print('ff.rho : ',ff.rho)
-print('ff.TT : ',ff.TT)
-
-T0 = np.mean(np.transpose(ff.T0),axis=1)
+T0 = np.mean(np.transpose(ff.TT),axis=1)
 TT = np.mean(np.transpose(ff.TT),axis=1)
+
+print('T0 : ',T0)
+print('TT : ',TT)
 
 plt.plot(rad,T0)
 plt.plot(rad,TT)
